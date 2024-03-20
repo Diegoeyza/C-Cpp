adf651660f8f383d4b3f2176f6ad0bb6355367e4
Diego <diegoeyzaguirreb@gmail.com>
2024-03-19 21:06:15 -0300

    Condición de término y duchas 1x1 norte sur

--- C/Test/T2.c
+++ C/Test/T2.c
@@ -1,4 +1,49 @@
 #include <stdio.h>
+#include <stdio.h>
+
+int id(piece){
+    int num;
+    if (piece=='a'||piece=='A'){
+        num=0;
+        return num;
+    }
+        else if (piece=='b'||piece=='B'){
+        num=1;
+        return num;
+    }
+        else if (piece=='c'||piece=='C'){
+        num=2;
+        return num;
+    }
+        else if (piece=='d'||piece=='D'){
+        num=3;
+        return num;
+    }
+        else if (piece=='e'||piece=='E'){
+        num=4;
+        return num;
+    }
+        else if (piece=='f'||piece=='F'){
+        num=5;
+        return num;
+    }
+        else if (piece=='g'||piece=='G'){
+        num=6;
+        return num;
+    }
+        else if (piece=='h'||piece=='H'){
+        num=7;
+        return num;
+    }
+        else if (piece=='i'||piece=='I'){
+        num=8;
+        return num;
+    }
+        else if (piece=='j'||piece=='J'){
+        num=9;
+        return num;
+    }
+}
 
 void Tablero(int Pieces[11][8]){
     char letters[]={"ABCDEFGHIJ  "};
@@ -41,23 +86,72 @@ void Tablero(int Pieces[11][8]){
 int main(){
 int Pieces[12][8]={{1,1,0,0,1,2,0,0},{2,2,3,3,1,2,1,2},{4,4,0,0,1,2,0,0},{1,0,0,0,3,0,0,0},{2,3,0,0,3,3,0,0},{4,0,0,0,3,0,0,0},{1,1,0,0,4,5,0,0},{2,0,0,0,4,0,0,0},{3,0,0,0,4,0,0,0},{4,4,0,0,4,5,0,0},{2,0,0,0,5,0,0,0},{3,0,0,0,5,0,0,0}}; //primeras 4 coordenadas de cada sublista es la posición en x de cada letra y las otras 4 la posición en y
 Tablero(Pieces);
+
 int exit=0;
-char ficha, sentido;
-int cantidad;
+char piece, direction;
+int ammount;
 while ((Pieces[1][0]!=2 && Pieces[1][1]!=2 && Pieces[1][2]!=3 && Pieces[1][3]!=3 && Pieces[1][4]!=4 && Pieces[1][5]!=4 && Pieces[1][6]!=5 && Pieces[1][7]!=5) || exit!=1){
     printf("Escriba la pieza que desea mover (a,b,c,d,e,f,g,h,i,j), la dirección (n,s,e,w) y la cantidad de movimientos (1 o 2). Si desea terminar escriba 0 como movimiento: ");
-    scanf("%c%c%d%*c", &ficha, &sentido, &cantidad);  //use el %*c para guardar como un input la tecla enter pero sin almacenarla en ni una parte (si no lo usaba el scanf se saltaba un ciclo)
-    if (cantidad==0){
+    scanf("%c%c%d%*c", &piece, &direction, &ammount);  //use el %*c para guardar como un input la tecla enter pero sin almacenarla en ni una parte (si no lo usaba el scanf se saltaba un ciclo)
+    if (ammount==0){
         exit=1;
 
     }
-    else if ((cantidad==1 || cantidad==2)&&(ficha=='a'||ficha=='A'||ficha=='b'||ficha=='B'||ficha=='c'||ficha=='C'||ficha=='d'||ficha=='D'||ficha=='e'||ficha=='E'||ficha=='f'||ficha=='F'||ficha=='g'||ficha=='G'||ficha=='h'||ficha=='H'||ficha=='i'||ficha=='I'||ficha=='j'||ficha=='J')&&(sentido=='n'||sentido=='N'||sentido=='s'||sentido=='S'||sentido=='e'||sentido=='E'||sentido=='w'||sentido=='W')){
-         
+    else if ((ammount==1 || ammount==2)&&(piece=='a'||piece=='A'||piece=='b'||piece=='B'||piece=='c'||piece=='C'||piece=='d'||piece=='D'||piece=='e'||piece=='E'||piece=='f'||piece=='F'||piece=='g'||piece=='G'||piece=='h'||piece=='H'||piece=='i'||piece=='I'||piece=='j'||piece=='J')&&(direction=='n'||direction=='N'||direction=='s'||direction=='S'||direction=='e'||direction=='E'||direction=='w'||direction=='W')){
+        int bank=0;
+        int pieza=id(piece);
+        if (pieza==3||pieza==5||pieza==7||pieza==8){
+            if (direction=='n'||direction=='N'){
+                if (ammount==1){
+                    if (Pieces[10][4]==Pieces[pieza][4]-1&&Pieces[pieza][4]-1!=0){
+                        Pieces [10][4]=bank;
+                        Pieces[10][4]=Pieces[pieza][4];
+                        Pieces[pieza][4]=bank;
+                    }
+                    else if (Pieces[11][4]==Pieces[pieza][4]-1&&Pieces[pieza][4]-1!=0){
+                        Pieces [11][4]=bank;
+                        Pieces[11][4]=Pieces[pieza][4];
+                        Pieces[pieza][4]=bank;
+                    }
+                    else{
+                        printf("Movimiento no válido");
+                    }
+                }
+                else if (ammount==2){
+                    if ((Pieces[10][4]==Pieces[pieza][4]-1&&Pieces[pieza][4]-2==Pieces[11][4])||(Pieces[11][4]==Pieces[pieza][4]-1&&Pieces[pieza][4]-2==Pieces[10][4])&&Pieces[pieza][4]+2>0)
+                        if (Pieces[10][4]>Pieces[11][4]){
+                            Pieces [11][4]=bank;
+                            Pieces[11][4]=Pieces[pieza][4];
+                            Pieces[pieza][4]=bank;
+                        }
+                        else if (Pieces[10][4]<Pieces[11][4]){
+                            Pieces [10][4]=bank;
+                            Pieces[10][4]=Pieces[pieza][4];
+                            Pieces[pieza][4]=bank;
+                        }
+                        else{
+                            printf("Error");
+                        }
+                }
+            }
+
+
+            else if (direction=='s'||direction=='S'){
+            }
+            else if (direction=='e'||direction=='E'){
+            }
+            else if (direction=='w'||direction=='W'){
+            }
     }
+
+
+
+}
     else{
         printf("\nInstrucción incorrecta \n");
 
     }
 }
+
 return 0;
 }
